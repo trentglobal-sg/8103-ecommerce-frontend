9d3835550e5216d0b00d138efec2928f17bcadcb
trentglobal-sg <paul.chor@trentglobal.edu.sg>
2026-02-09 07:48:05 +0000

    Added routing for products and register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import React, { useState } from 'react';
+import {Switch, Route} from 'wouter';
+
 import ProductCard from './ProductCard';
 import Navbar from './Navbar';
 import MainPage from './MainPage';
-import {Switch, Route} from 'wouter';
+import RegisterPage from './RegisterPage';
+import ProductPage from './ProductPage';
+
 
 export default function App() {
-
-  
-
-
 
   return (<>
     <Navbar/>
 
     <Switch>
       <Route path="/" component={MainPage}/>
+      <Route path="/products" component={ProductPage}/>
+      <Route path="/register" component={RegisterPage}/>
     </Switch>
 
     <footer className="bg-dark text-white text-center py-3">
---
+++ src/ProductPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ProductPage() {
+    return (<>
+        <h1>Product Page</h1>
+    </>)
+}
