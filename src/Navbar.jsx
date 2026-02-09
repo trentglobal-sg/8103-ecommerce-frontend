75db58aac34f18cf732c95006b21a701883ac249
trentglobal-sg <paul.chor@trentglobal.edu.sg>
2026-02-09 07:50:32 +0000

    Added nav bar links

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import { Link } from 'wouter';
 
 export default function Navbar() {
 
@@ -11,7 +12,7 @@ export default function Navbar() {
     return (<>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container">
-                <a className="navbar-brand" href="#">E-Shop</a>
+                <Link className="navbar-brand" href="/">E-Shop</Link>
                 <button
                     className="navbar-toggler"
                     type="button"
@@ -25,16 +26,13 @@ export default function Navbar() {
                 <div className={`collapse navbar-collapse ${isNavbarShowing ? "show" : ""}`} id="navbarNav">
                     <ul className="navbar-nav ms-auto">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <Link className="nav-link active" aria-current="page" href="/">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Products</a>
+                            <Link className="nav-link" href="/products">Products</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">About</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Contact</a>
+                            <Link className="nav-link" href="/register">Register</Link>
                         </li>
                     </ul>
                 </div>
